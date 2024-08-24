7ec8d4eb8a9fce6b484fa2aad8085514f33cf1d4
afeiship <1290657123@qq.com>
2024-08-24 22:08:07 +0800

    feat: fly2cart with name + cmds

--- packages/example/src/app.tsx
+++ packages/example/src/app.tsx
@@ -18,14 +18,19 @@ function App() {
       <ReactFly2Cart target=".the-cart" className="abs top-100 left-100" onClick={() => setNum(num + 1)}>
         <button className="btn btn-sm btn-primary">Buy Now</button>
       </ReactFly2Cart>
-      <ReactFly2Cart name="minus" target=".the-cart" manually className="abs top-50 right-100"
-                     onClick={() => {
-                       setNum(num - 1);
-                       setTimeout(() => {
-                         console.log('fly2cart');
-                         fly2cart();
-                       }, 1000);
-                     }}>
+      <ReactFly2Cart
+        name="minus"
+        target=".the-cart"
+        manually
+        className="abs top-50 right-100"
+        onClick={() => {
+          setNum(num - 1);
+          setTimeout(() => {
+            console.log('fly2cart');
+            fly2cart();
+          }, 1000);
+        }}
+      >
         <button className="btn btn-sm btn-secondary">Buy immediately(-1)</button>
       </ReactFly2Cart>
     </div>
