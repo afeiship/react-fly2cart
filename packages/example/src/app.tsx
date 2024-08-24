699d91f9207da0292fc27f1aeac9c175142ade46
afeiship <1290657123@qq.com>
2024-08-24 20:26:58 +0800

    feat: basic fly efffect

--- packages/example/src/app.tsx
+++ packages/example/src/app.tsx
@@ -3,12 +3,13 @@ import '@jswork/react-fly2cart/src/style.scss';
 
 function App() {
   return (
-    <div className="m-10 p-4 shadow bg-gray-100 text-gray-800 hover:shadow-md transition-all">
+    <div className="m-10 p-4 shadow bg-gray-100 text-gray-800 h-100 hover:shadow-md transition-all">
       <div className="badge badge-warning absolute right-0 top-0 m-4">
         Build Time: {BUILD_TIME}
       </div>
-      <ReactFly2Cart className="debug-red">
-        abc
+      <div className="the-cart bg-green-400 size-16 rounded-full flex justify-center items-center corner-r right-10 abs">List: 0</div>
+      <ReactFly2Cart target=".the-cart" className="abs top-100 left-100">
+        <button className="btn btn-sm btn-primary">Buy Now</button>
       </ReactFly2Cart>
     </div>
   );
--- packages/lib/src/index.tsx
+++ packages/lib/src/index.tsx
@@ -1,18 +1,25 @@
-// import noop from '@jswork/noop';
 import cx from 'classnames';
-import React, { ReactNode, Component, HTMLAttributes } from 'react';
+import React, { Component, HTMLAttributes } from 'react';
 
 const CLASS_NAME = 'react-fly2cart';
-// const uuid = () => Math.random().toString(36).substring(2, 9);
+const createBall = (className: string) => {
+  const ball = document.createElement('div');
+  ball.id = 'ball';
+  ball.className = cx(className, 'react-fly2cart-ball');
+  ball.innerHTML = '<div class="react-fly2cart-ball-inner"></div>';
+  document.body.appendChild(ball);
+  return ball;
+};
+
 export type ReactFly2CartProps = {
   /**
    * Create a ball element.
    */
-  createBall?: () => HTMLElement;
+  createBall?: (className: string) => HTMLElement;
   /**
-   * The source element or selector.
+   * The ball class name.
    */
-  source: string | HTMLElement;
+  ballClassName?: string;
   /**
    * The target element or selector.
    */
@@ -22,14 +29,44 @@ export type ReactFly2CartProps = {
 export default class ReactFly2Cart extends Component<ReactFly2CartProps> {
   static displayName = CLASS_NAME;
   static version = '__VERSION__';
-  static defaultProps = {};
+  static defaultProps = {
+    createBall: createBall,
+    ballClassName: '',
+  };
+
+  private rootDom: HTMLSpanElement | null = null;
+
+  componentDidMount() {
+    console.log('componentDidMount: ', this.rootDom);
+  }
+
+  handleClick = () => {
+    const { createBall, ballClassName, target } = this.props;
+    const root = this.rootDom as HTMLElement;
+    const targetEl = typeof target === 'string' ? document.querySelector(target) : target;
+    const rootBound = root.getBoundingClientRect();
+    const cartBound = targetEl?.getBoundingClientRect()!;
+    const ball = createBall?.(ballClassName as string) as HTMLElement;
+
+    const left = rootBound.left + rootBound.width / 2 - ball.offsetWidth / 2;
+    const top = rootBound.top + rootBound.height / 2 - ball.offsetHeight / 2;
+    const x = cartBound.left + cartBound.width / 2;
+    const y = cartBound.top + cartBound.height / 2 - rootBound.top;
+    ball.style.cssText = `--left: ${left}px; --top: ${top}px; --x: ${x}px; --y: ${y}px;`;
+    ball.onanimationend = () => ball.remove();
+  };
 
   render() {
-    const { className, children, ...rest } = this.props;
+    const { className, children, createBall, ballClassName, target, onClick, ...rest } = this.props;
     return (
-      <div data-component={CLASS_NAME} className={cx(CLASS_NAME, className)} {...rest}>
+      <span
+        data-component={CLASS_NAME}
+        className={cx(CLASS_NAME, className)}
+        onClick={this.handleClick}
+        ref={(el) => (this.rootDom = el)}
+        {...rest}>
         {children}
-      </div>
+      </span>
     );
   }
 }
