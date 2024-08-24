0b2b24ce74232aa76071baf2f6bd5df85b54fde4
afeiship <1290657123@qq.com>
2024-08-24 19:27:26 +0800

    feat: init styles

--- packages/lib/src/index.tsx
+++ packages/lib/src/index.tsx
@@ -1,28 +1,31 @@
 // import noop from '@jswork/noop';
 import cx from 'classnames';
-import React, { ReactNode, Component, HTMLAttributes } from "react";
+import React, { ReactNode, Component, HTMLAttributes } from 'react';
 
-const CLASS_NAME = "react-fly2cart";
+const CLASS_NAME = 'react-fly2cart';
 // const uuid = () => Math.random().toString(36).substring(2, 9);
 export type ReactFly2CartProps = {
   /**
-   * The extended className for component.
-   * @default ''
+   * Create a ball element.
    */
-  className?: string;
+  createBall?: () => HTMLElement;
   /**
-   * The children element.
+   * The source element or selector.
    */
-  children?: ReactNode;
+  source: string | HTMLElement;
+  /**
+   * The target element or selector.
+   */
+  target: string | HTMLElement;
 } & HTMLAttributes<HTMLDivElement>;
 
 export default class ReactFly2Cart extends Component<ReactFly2CartProps> {
   static displayName = CLASS_NAME;
-  static version = "__VERSION__";
+  static version = '__VERSION__';
   static defaultProps = {};
 
   render() {
-    const { className, children,...rest } = this.props;
+    const { className, children, ...rest } = this.props;
     return (
       <div data-component={CLASS_NAME} className={cx(CLASS_NAME, className)} {...rest}>
         {children}
