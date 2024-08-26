745133079b81cb82a2f6d4bc541eed9256ae207c
afeiship <1290657123@qq.com>
2024-08-26 21:29:26 +0800

    fix: ball position

--- packages/lib/src/index.tsx
+++ packages/lib/src/index.tsx
@@ -36,7 +36,7 @@ export default class ReactFly2Cart extends Component<ReactFly2CartProps> {
   static defaultProps = {
     name: '@',
     manually: false,
-    direction: 'top'
+    direction: 'top',
   };
 
   private rootDom: HTMLSpanElement | null = null;
@@ -71,11 +71,10 @@ export default class ReactFly2Cart extends Component<ReactFly2CartProps> {
     const root = this.rootDom as HTMLElement;
     const rootBound = root.getBoundingClientRect();
     const cartBound = this.targetDom?.getBoundingClientRect()!;
-    const ballBound = ball.getBoundingClientRect();
 
     const left = rootBound.left + rootBound.width / 2 - ball.offsetWidth / 2;
     const top = rootBound.top + rootBound.height / 2 - ball.offsetHeight / 2;
-    const x = cartBound.left - rootBound.left - ballBound.width / 2;
+    const x = -(rootBound.left + rootBound.width / 2 - cartBound.left - cartBound.width / 2);
     const y = -(rootBound.top + rootBound.height / 2 - cartBound.top - cartBound.height / 2);
     ball.style.cssText = `--left: ${left}px; --top: ${top}px; --x: ${x}px; --y: ${y}px;`;
     ball.onanimationend = () => {
